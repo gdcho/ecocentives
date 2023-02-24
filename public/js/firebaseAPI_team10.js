783d31faeb2b3a88dc4384c18b8bc1fd08340f2f
Gin Lu <glu23@my.bcit.ca>
2023-02-24 12:43:24 -0800

    Added main.html, main.js and did updates - Gin

---
+++ public/js/firebaseAPI_team10.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRDpX0J5MhrUoXb3IrLe8d2nRa10KC1Gs",
+  authDomain: "comp1800-202310-3a4f1.firebaseapp.com",
+  projectId: "comp1800-202310-3a4f1",
+  storageBucket: "comp1800-202310-3a4f1.appspot.com",
+  messagingSenderId: "98352856971",
+  appId: "1:98352856971:web:8937db7ec1c6c5d88b798e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

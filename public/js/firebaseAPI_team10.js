f15db0b2f21f285933aca5e9bca0cca3e647fe03
Gin Lu <glu23@my.bcit.ca>
2023-02-28 11:36:04 -0800

    Updated sign in to reflect Firebase UI Auth - Gin

--- public/js/firebaseAPI_team10.js
+++ public/js/firebaseAPI_team10.js
@@ -1,10 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
   apiKey: "AIzaSyDRDpX0J5MhrUoXb3IrLe8d2nRa10KC1Gs",
   authDomain: "comp1800-202310-3a4f1.firebaseapp.com",
   projectId: "comp1800-202310-3a4f1",
@@ -13,5 +10,10 @@ const firebaseConfig = {
   appId: "1:98352856971:web:8937db7ec1c6c5d88b798e"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const storage = firebase.storage();

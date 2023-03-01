500ee14443f18127f5160c294e67bfcbe44d7764
Gin Lu <glu23@my.bcit.ca>
2023-02-28 20:19:28 -0800

    Added navbar to pages and did code updates - Gin

--- public/js/main.js
+++ public/js/main.js
@@ -18,3 +18,20 @@ function insertName() {
     });
 }
 insertName(); //run the function
+
+// Function to read the quote of the day from Firestore "quotes" collection
+// Input param is the String representing the day of the week, aka, the document name
+function readQuote(day) {
+    db.collection("quotes").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
+      .onSnapshot(mondayDoc => {                                                               //arrow notation
+           console.log("current document data: " + mondayDoc.data());                          //.data() returns data object
+           document.getElementById("quote-goes-here").innerHTML = mondayDoc.data().quote;      //using javascript to display the data on the right place
+           
+           //Here are other ways to access key-value data fields
+           //$('#quote-goes-here').text(tuesdayDoc.data().quote);         //using jquery object dot notation
+           //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);      //using json object indexing
+		       //document.querySelector("#quote-goes-here").innerHTML = tuesdayDoc.data().quote;
+      })
+}
+readQuote("monday");        //calling the function
+

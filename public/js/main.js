3a4263b2611201150a101b3e6816359acb4bd52e
David Cho <gcho13@my.bcit.ca>
2023-03-03 11:05:52 -0800

    points added on main.js - david

--- public/js/main.js
+++ public/js/main.js
@@ -35,4 +35,20 @@ function readQuote(day) {
 }
 readQuote("monday");        //calling the function
 
+function readPoint() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            db.collection("users").doc(user.uid)
+                .onSnapshot(doc => {
+                    console.log(doc.data());
+                    const userPoint = doc.data().point;
+                    document.getElementById("point-goes-here").innerHTML = userPoint;
+                });
+        } else {
+        }
+    });
+}
+readPoint(); // call the function
+
 

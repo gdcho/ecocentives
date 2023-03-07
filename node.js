d0f763fea47479ed21d48ecd79692bad67bbf449
Gin Lu <glu23@my.bcit.ca>
2023-03-06 21:38:50 -0800

    Added in additional app.get path for index - Gin

--- node.js
+++ node.js
@@ -1,6 +1,3 @@
-// https://expressjs.com/en/guide/routing.html
-
-
 // REQUIRES
 const express = require("express");
 const app = express();
@@ -15,8 +12,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// just like a simple web server like Apache web server
-// we are mapping file system paths to the app's virtual paths
 app.use(express.static('app'));
 app.use("/js", express.static("./public/js"));
 app.use("/css", express.static("./public/css"));
@@ -27,12 +22,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/app/html/index.html');
   });
 
-app.get('/habit.html', (req, res) => {
-    res.sendFile(__dirname + '/app/html/habit.html');
+  app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/app/html/index.html');
   });
 
 app.get('/about.html', (req, res) => {
     res.sendFile(__dirname + '/app/html/about.html');
+  });
+
+  app.get('/habit.html', (req, res) => {
+    res.sendFile(__dirname + '/app/html/habit.html');
   });
   
 app.get('/leaderboard.html', (req, res) => {
@@ -71,12 +70,10 @@ app.get('/main.html', (req, res) => {
   res.sendFile(__dirname + '/app/html/main.html');
 });
 
-// // for resource not found (i.e., 404)
-// app.use(function (req, res, next) {
-//     // this could be a separate file too - but you'd have to make sure that you have the path
-//     // correct, otherewise, you'd get a 404 on the 404 (actually a 500 on the 404)
-//     res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
-// });
+// For resource not found (i.e., 404)
+app.use(function (req, res, next) {
+    res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");
+});
 
 // RUN SERVER
 app.listen(8000, () => {

11536a3ae7ef1da03259a5593c82d7231c3da683
Gin Lu <glu23@my.bcit.ca>
2023-03-08 09:56:39 -0800

    Updated DocID String - Gin

--- public/js/rewards.js
+++ public/js/rewards.js
@@ -1,5 +1,4 @@
 function saveRedemptionDocumentIDAndRedirect() {
-    const docID = Date.now().toString() + Math.floor(Math.random() * 100000).toString();
     localStorage.setItem('redemptionDocID', docID);
     window.location.href = 'redemption.html';
   }

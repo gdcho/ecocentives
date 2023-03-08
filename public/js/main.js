1be77e35bec61a22202cf1988e8d0709a9a82870
Gin Lu <glu23@my.bcit.ca>
2023-03-07 21:59:16 -0800

    Some comments clean up - Gin

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,6 @@ function insertName() {
             console.log(user.uid); //print the uid in the browser console
             console.log(user.displayName);  //print the user name in the browser console
             user_Name = user.displayName;
-
-            //method #1:  insert with html only
-            //document.getElementById("name-goes-here").innerText = user_Name;    //using javascript
-            //method #2:  insert using jquery
             $("#name-goes-here").text(user_Name); //using jquery
 
         } else {
@@ -17,23 +13,18 @@ function insertName() {
         }
     });
 }
-insertName(); //run the function
+insertName();
 
 // Function to read the quote of the day from Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
-    db.collection("quotes").doc(day)                                                      //name of the collection and documents should matach excatly with what you have in Firestore
-      .onSnapshot(mondayDoc => {                                                               //arrow notation
-           console.log("current document data: " + mondayDoc.data());                          //.data() returns data object
-           document.getElementById("quote-goes-here").innerHTML = mondayDoc.data().quote;      //using javascript to display the data on the right place
-           
-           //Here are other ways to access key-value data fields
-           //$('#quote-goes-here').text(tuesdayDoc.data().quote);         //using jquery object dot notation
-           //$("#quote-goes-here").text(tuesdayDoc.data()["quote"]);      //using json object indexing
-		       //document.querySelector("#quote-goes-here").innerHTML = tuesdayDoc.data().quote;
-      })
+    db.collection("quotes").doc(day)
+        .onSnapshot(mondayDoc => {
+            console.log("current document data: " + mondayDoc.data());
+            document.getElementById("quote-goes-here").innerHTML = mondayDoc.data().quote;
+        })
 }
-readQuote("monday");        //calling the function
+readQuote("monday");
 
 function readPoint() {
     firebase.auth().onAuthStateChanged(user => {
@@ -49,6 +40,6 @@ function readPoint() {
         }
     });
 }
-readPoint(); // call the function
+readPoint();
 
 

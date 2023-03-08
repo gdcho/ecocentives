5b745915f98b6f89063c6f93b516e2eeb4d1d320
Gin Lu <glu23@my.bcit.ca>
2023-03-08 15:19:38 -0800

    Defined docID in rewards.js - Gin

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,45 @@
 function insertName() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            // Do something for the currently logged-in user here: 
-            console.log(user.uid); //print the uid in the browser console
-            console.log(user.displayName);  //print the user name in the browser console
-            user_Name = user.displayName;
-            $("#name-goes-here").text(user_Name); //using jquery
-
-        } else {
-            // No user is signed in.
-        }
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      // Do something for the currently logged-in user here:
+      console.log(user.uid); //print the uid in the browser console
+      console.log(user.displayName); //print the user name in the browser console
+      user_Name = user.displayName;
+      $("#name-goes-here").text(user_Name); //using jquery
+    } else {
+      // No user is signed in.
+    }
+  });
 }
 insertName();
 
 // Function to read the quote of the day from Firestore "quotes" collection
 // Input param is the String representing the day of the week, aka, the document name
 function readQuote(day) {
-    db.collection("quotes").doc(day)
-        .onSnapshot(mondayDoc => {
-            console.log("current document data: " + mondayDoc.data());
-            document.getElementById("quote-goes-here").innerHTML = mondayDoc.data().quote;
-        })
+  db.collection("quotes")
+    .doc(day)
+    .onSnapshot((mondayDoc) => {
+      console.log("current document data: " + mondayDoc.data());
+      document.getElementById("quote-goes-here").innerHTML =
+        mondayDoc.data().quote;
+    });
 }
 readQuote("monday");
 
 function readPoint() {
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            db.collection("users").doc(user.uid)
-                .onSnapshot(doc => {
-                    console.log(doc.data());
-                    const userPoint = doc.data().point;
-                    document.getElementById("point-goes-here").innerHTML = userPoint;
-                });
-        } else {
-        }
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if a user is signed in:
+    if (user) {
+      db.collection("users")
+        .doc(user.uid)
+        .onSnapshot((doc) => {
+          console.log(doc.data());
+          const userPoint = doc.data().point;
+          document.getElementById("point-goes-here").innerHTML = userPoint;
+        });
+    } else {
+    }
+  });
 }
 readPoint();
-
-
--- public/js/rewards.js
+++ public/js/rewards.js
@@ -1,5 +1,20 @@
-function saveRedemptionDocumentIDAndRedirect() {
-    localStorage.setItem('redemptionDocID', docID);
-    window.location.href = 'redemption.html';
-  }
-  
+const rewardsRef = firebase.firestore().collection("rewards");
+function getRewardsData() {
+  rewardsRef.get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      const data = doc.data();
+      const name = data.name;
+      const points = data.points;
+      const description = data.description;
+      console.log(
+        `Reward Name: ${name}, Points Required: ${points}, Description: ${description}`
+      );
+    });
+  });
+}
+getRewardsData();
+
+function saveRedemptionDocumentIDAndRedirect(docID) {
+  localStorage.setItem("redemptionDocID", docID);
+  window.location.href = "redemption.html";
+}

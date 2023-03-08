77da5ebbb4705c4f4dc5b4a0b3286c9f8b773d08
Gin Lu <glu23@my.bcit.ca>
2023-03-07 21:40:28 -0800

    Updated to write redemptionDocID to Firebase - Gin

--- public/js/rewards.js
+++ public/js/rewards.js
@@ -1,6 +1,6 @@
-function saveRedemptionDocumentIDAndRedirect(){
-    let params = new URL(window.location.href) 
-    let ID = params.searchParams.get("docID");
-    localStorage.setItem('redemptionDocID', ID);
+function saveRedemptionDocumentIDAndRedirect() {
+    const docID = Date.now().toString() + Math.floor(Math.random() * 100000).toString();
+    localStorage.setItem('redemptionDocID', docID);
     window.location.href = 'redemption.html';
-}
+  }
+  

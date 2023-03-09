28343c91f5e096eca881fd256430eecfb033d77d
David Cho <gcho13@my.bcit.ca>
2023-03-08 16:46:19 -0800

    trying to fix beforelogin nav - david

--- public/js/firebaseAPI_team10.js
+++ public/js/firebaseAPI_team10.js
@@ -16,4 +16,4 @@ var firebaseConfig = {
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-// const storage = firebase.storage();
+//const storage = firebase.storage();

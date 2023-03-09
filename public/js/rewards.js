58d50e6a2d79856842355d143473c68ce1ad2b52
Gin Lu <glu23@my.bcit.ca>
2023-03-08 16:48:46 -0800

    Started updating rewards and its functions - Gin

--- public/js/rewards.js
+++ public/js/rewards.js
@@ -1,19 +1,45 @@
-const rewardsRef = firebase.firestore().collection("rewards");
-function getRewardsData() {
-  rewardsRef.get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      const name = data.name;
-      const points = data.points;
-      const description = data.description;
-      console.log(
-        `Reward Name: ${name}, Points Required: ${points}, Description: ${description}`
-      );
-    });
-  });
-}
-getRewardsData();
+const datastore = firebase.firestore();
 
+function getRewardsData(rewardName, rewardContainer) {
+  const rewardsRef = datastore.collection("rewards");
+  rewardsRef.where("name", "==", rewardName)
+    .get()
+    .then((querySnapshot) => {
+      if (querySnapshot.empty) {
+        console.log('No matching documents.');
+      } else {
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const points = data.points;
+          const description = data.description;
+
+          const rewardNameElem = rewardContainer.querySelector('.reward-name');
+          rewardNameElem.textContent = rewardName;
+
+          const pointsElem = rewardContainer.querySelector('.points');
+          pointsElem.textContent = `${points} points`;
+
+          const descriptionElem = rewardContainer.querySelector('.description');
+          descriptionElem.textContent = description;
+        });
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting reward document: ", error);
+    });
+}
+
+const amazonRewardContainer = document.querySelector('#amazon');
+getRewardsData("$100 Amazon Gift Card", amazonRewardContainer);
+
+const bestbuyRewardContainer = document.querySelector('#bestbuy');
+getRewardsData("$100 Best Buy Gift Card", bestbuyRewardContainer);
+
+const visaRewardContainer = document.querySelector('#visa');
+getRewardsData("$100 Visa Gift Card", visaRewardContainer);
+
+
+// Creates a redemptionDocID in Firestore Database.
 function saveRedemptionDocumentIDAndRedirect(docID) {
   localStorage.setItem("redemptionDocID", docID);
   window.location.href = "redemption.html";

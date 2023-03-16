79f5928a586e5e93a50bc76daf9567b41ff72c3b
David Cho <gcho13@my.bcit.ca>
2023-03-16 00:50:50 -0700

    added task array linked to firebase - david

--- public/js/main.js
+++ public/js/main.js
@@ -43,3 +43,100 @@ function readPoints() {
   });
 }
 readPoints();
+
+const ecoActions = [
+  {
+    action: "Using a clothesline to dry clothes instead of a dryer",
+    score: 20
+  },
+  {
+    action: "Repairing or repurposing items instead of throwing them away",
+    score: 30
+  },
+  {
+    action: "Using a programmable thermostat to conserve energy",
+    score: 20
+  },
+  {
+    action: "Avoiding single-use plastics",
+    score: 10
+  },
+  {
+    action: "Supporting sustainable farming and fishing practices",
+    score: 20
+  },
+  {
+    action: "Using non-toxic cleaning products",
+    score: 10
+  },
+  {
+    action: "Supporting politicians and policies that prioritize environmental protection",
+    score: 40
+  },
+  {
+    action: "Participating in community clean-up events",
+    score: 30
+  },
+  {
+    action: "Using a bidet or reusable cloth wipes instead of disposable toilet paper",
+    score: 20
+  },
+  {
+    action: "Reducing meat consumption and adopting a plant-based diet",
+    score: 30
+  },
+  {
+    action: "Using a reusable water bottle instead of disposable plastic bottles",
+    score: 10
+  },
+  {
+    action: "Taking public transportation, biking or walking instead of driving a car",
+    score: 20
+  },
+  {
+    action: "Planting a tree or starting a garden",
+    score: 30
+  },
+  {
+    action: "Using energy-efficient appliances and light bulbs",
+    score: 10
+  },
+  {
+    action: "Buying organic and locally-sourced foods",
+    score: 20
+  },
+  {
+    action: "Composting food waste instead of throwing it away",
+    score: 30
+  },
+  {
+    action: "Installing solar panels or using other renewable energy sources",
+    score: 40
+  },
+  {
+    action: "Using cloth bags instead of plastic bags",
+    score: 10
+  },
+  {
+    action: "Turning off lights and electronics when not in use",
+    score: 10
+  },
+  {
+    action: "Supporting companies with eco-friendly practices and policies",
+    score: 20
+  }
+];
+
+function writeEcoActions() {
+  var ecoActionsRef = db.collection("ecoActions");
+
+  ecoActions.forEach(action => {
+    ecoActionsRef.add({
+      action: action.action,
+      score: action.score,
+    });
+  });
+}
+
+writeEcoActions();
+

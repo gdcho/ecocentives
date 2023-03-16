a9cc954519c9306af88f84fca78377991c08d8ef
Gin Lu <glu23@my.bcit.ca>
2023-03-15 18:01:40 -0700

    Updated settings.html and CSS files

---
+++ public/js/settings.js
@@ -0,0 +1,6 @@
+const saveBtn = document.getElementById("save-settings-btn");
+
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  alert("Settings saved successfully!");
+});

2f83bfc88e036e9bc1dfbe58bef8ab79eebaf767
Sohee Hwang <shwang67@my.bcit.ca>
2023-03-17 16:30:57 -0700

    chagne habit.js

--- public/js/habit.js
+++ public/js/habit.js
@@ -2,38 +2,61 @@ var firebaseConfig = {
   // Your Firebase project configuration details
 };
 
+var taskTracker = document.getElementById("task-tracker");
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var currentUser = db.collection("users").doc(user.uid);
+    currentUser.collection("tasks").onSnapshot(function(snapshot) {
+      taskTracker.innerHTML = "";
+      snapshot.forEach(function(doc) {
+        var task = doc.data().task;
+        var frequency = doc.data().frequency;
+        var progress = doc.data().progress;
+
+        var row = taskTracker.insertRow();
+        var taskCell = row.insertCell(0);
+        var frequencyCell = row.insertCell(1);
+        var progressCell = row.insertCell(2);
+        taskCell.innerHTML = task;
+        frequencyCell.innerHTML = frequency;
+        progressCell.innerHTML = progress + "%";
+
+        row.addEventListener("click", function() {
+          var newProgress = progress + 10;
+          if (newProgress > 100) {
+            newProgress = 0;
+          }
+          doc.ref.update({
+            progress: newProgress
+          });
+        });
+      });
+    });
+  }
+});
+
 function addTask() {
-  // Get the selected values from the dropdowns
   var taskList = document.getElementById("task-list");
   var taskChoice = taskList.options[taskList.selectedIndex].text;
   var frequencyList = document.getElementById("frequency");
   var frequencyChoice = frequencyList.options[frequencyList.selectedIndex].text;
 
-  //내 로그인 되어있으면
-  firebase.auth().onAuthStateChanged((user) => {
-      console.log('task');
-      console.log(taskChoice);
-      var currentUser = db.collection("users").doc(user.uid);
-      var userUID = user.uid;
-     
-      //Add the selected values to the Firebase database
-      db.collection("users").doc(userUID).update({
-        task: taskChoice,
-        frequency:frequencyChoice
-      }).then(function(){
-        console.log("test success");
-      }).catch(function(error){
-        console.log('error' +error);
-      })
-  });
+  var user = firebase.auth().currentUser;
+  if (!user) {
+    return;
+  }
 
-  // Add the selected values to the table
-  var table = document.getElementById("task-tracker");
-  var row = table.insertRow();
-  var taskCell = row.insertCell(0);
-  var frequencyCell = row.insertCell(1);
-  var progressCell = row.insertCell(2);
-  taskCell.innerHTML = taskChoice;
-  frequencyCell.innerHTML = frequencyChoice;
-  progressCell.innerHTML = "0%";
+  var currentUser = db.collection("users").doc(user.uid);
+  currentUser.collection("tasks").add({
+    task: taskChoice,
+    frequency: frequencyChoice,
+    progress: 0
+  }).then(function(docRef) {
+    console.log("Task added with ID: ", docRef.id);
+  }).catch(function(error) {
+    console.error("Error adding task: ", error);
+  });
 }
+
+

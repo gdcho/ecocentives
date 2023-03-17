f044ee1aad1d3f4669f9cd62d53c58266016d100
Sohee Hwang <shwang67@my.bcit.ca>
2023-03-17 16:47:06 -0700

    habit.js

--- public/js/habit.js
+++ public/js/habit.js
@@ -3,11 +3,11 @@ var firebaseConfig = {
 };
 
 var taskTracker = document.getElementById("task-tracker");
+var tasksCollection = db.collection("tasks");
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    var currentUser = db.collection("users").doc(user.uid);
-    currentUser.collection("tasks").onSnapshot(function(snapshot) {
+    tasksCollection.where("userId", "==", user.uid).onSnapshot(function(snapshot) {
       taskTracker.innerHTML = "";
       snapshot.forEach(function(doc) {
         var task = doc.data().task;
@@ -47,15 +47,52 @@ function addTask() {
     return;
   }
 
-  var currentUser = db.collection("users").doc(user.uid);
-  currentUser.collection("tasks").add({
+  tasksCollection.add({
     task: taskChoice,
     frequency: frequencyChoice,
-    progress: 0
+    progress: 0,
+    userId: user.uid
   }).then(function(docRef) {
     console.log("Task added with ID: ", docRef.id);
   }).catch(function(error) {
     console.error("Error adding task: ", error);
   });
 }
+
+// Remember tasks on page refresh
+window.addEventListener("load", function() {
+  var user = firebase.auth().currentUser;
+  if (!user) {
+    return;
+  }
+
+  tasksCollection.where("userId", "==", user.uid).get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      var task = doc.data().task;
+      var frequency = doc.data().frequency;
+      var progress = doc.data().progress;
+
+      var row = taskTracker.insertRow();
+      var taskCell = row.insertCell(0);
+      var frequencyCell = row.insertCell(1);
+      var progressCell = row.insertCell(2);
+      taskCell.innerHTML = task;
+      frequencyCell.innerHTML = frequency;
+      progressCell.innerHTML = progress + "%";
+
+      row.addEventListener("click", function() {
+        var newProgress = progress + 10;
+        if (newProgress > 100) {
+          newProgress = 0;
+        }
+        doc.ref.update({
+          progress: newProgress
+        });
+      });
+    });
+  });
+});
+
+
+
 

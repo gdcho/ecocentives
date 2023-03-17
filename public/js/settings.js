241231f6f56f5b40af6998a139188caa99bc22c9
Gin Lu <glu23@my.bcit.ca>
2023-03-16 19:32:11 -0700

    Moved logout button to the right of navbar

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,47 +1,47 @@
-const user = firebase.auth().currentUser;
+/* Saving user settings in a subcollection for the user. */
+firebase.auth().onAuthStateChanged(async function (user) {
+  if (user) {
+    const userId = user.uid;
+    const userRef = db.collection("users").doc(userId);
+    const settingsRef = userRef.collection("settings");
+    const saveBtn = document.getElementById("save-settings-btn");
 
-if (user) {
-  const userId = user.uid;
-  const userRef = db.collection("users").doc(userId);
-  const settingsRef = userRef.collection("settings");
-  const saveBtn = document.getElementById("save-settings-btn");
+    saveBtn.addEventListener("click", async (event) => {
+      event.preventDefault();
 
-  saveBtn.addEventListener("click", (event) => {
-    event.preventDefault();
+      const newRewards = document.getElementById("new-rewards").checked;
+      const recyclingReminders = document.getElementById(
+        "recycling-reminders"
+      ).checked;
+      const progressUpdates =
+        document.getElementById("progress-updates").checked;
+      const notificationFrequency = document.getElementById(
+        "notification-frequency"
+      ).value;
+      const connectFacebook =
+        document.getElementById("connect_facebook").checked;
+      const connectInstagram =
+        document.getElementById("connect_instagram").checked;
+      const connectTwitter = document.getElementById("connect_twitter").checked;
 
-    const newRewards = document.getElementById("new-rewards").checked;
-    const recyclingReminders = document.getElementById(
-      "recycling-reminders"
-    ).checked;
-    const progressUpdates = document.getElementById("progress-updates").checked;
-    const notificationFrequency = document.getElementById(
-      "notification-frequency"
-    ).value;
-    const connectFacebook = document.getElementById("connect_facebook").checked;
-    const connectInstagram =
-      document.getElementById("connect_instagram").checked;
-    const connectTwitter = document.getElementById("connect_twitter").checked;
-
-    settingsRef
-      .doc("userSettings")
-      .set({
-        newRewards: newRewards,
-        recyclingReminders: recyclingReminders,
-        progressUpdates: progressUpdates,
-        notificationFrequency: notificationFrequency,
-        connectFacebook: connectFacebook,
-        connectInstagram: connectInstagram,
-        connectTwitter: connectTwitter,
-      })
-      .then(() => {
+      try {
+        await settingsRef.doc("userSettings").set({
+          newRewards: newRewards,
+          recyclingReminders: recyclingReminders,
+          progressUpdates: progressUpdates,
+          notificationFrequency: notificationFrequency,
+          connectFacebook: connectFacebook,
+          connectInstagram: connectInstagram,
+          connectTwitter: connectTwitter,
+        });
         console.log("Settings saved successfully!");
         alert("Settings saved successfully!");
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error saving settings: ", error);
-      });
-  });
-} else {
-  // User is not authenticated
-  console.log("User is not authenticated.");
-}
+      }
+    });
+  } else {
+    // User is not authenticated.
+    console.log("User is not authenticated.");
+  }
+});

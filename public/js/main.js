662b5f842e0e76508cfbc1e321459f5ec2537b56
Gin Lu <glu23@my.bcit.ca>
2023-03-17 19:24:26 -0700

    Added ecotips for each day of the week

--- public/js/main.js
+++ public/js/main.js
@@ -14,17 +14,26 @@ function insertName() {
 insertName();
 
 /* Display quote from Firestore Database. */
-function readQuote(day) {
-  db.collection("quotes")
-    .doc(day)
-    .onSnapshot((mondayDoc) => {
-      console.log("current document data: " + mondayDoc.data());
-      document.getElementById("quote-goes-here").innerHTML =
-        mondayDoc.data().quote;
+function readEcoTip() {
+  const dayOfWeek = new Date().toLocaleString("en-us", { weekday: "long" });
+  const tipRef = db.collection("ecoTips").doc(dayOfWeek);
+  tipRef
+    .get()
+    .then((tipDoc) => {
+      if (tipDoc.exists) {
+        document.getElementById("ecotip-goes-here").innerHTML =
+          tipDoc.data().tip;
+      } else {
+        console.log("No tip found for " + dayOfWeek);
+      }
+    })
+    .catch((error) => {
+      console.error("Error getting tip: ", error);
     });
 }
-readQuote("monday");
+readEcoTip();
 
+/* Display points from Firestore Database. */
 function readPoints() {
   firebase.auth().onAuthStateChanged((user) => {
     // Check if a user is signed in:
@@ -45,84 +54,86 @@ readPoints();
 const ecoActions = [
   {
     action: "Using a clothesline to dry clothes instead of a dryer",
-    score: 20
+    score: 20,
   },
   {
     action: "Repairing or repurposing items instead of throwing them away",
-    score: 30
+    score: 30,
   },
   {
     action: "Using a programmable thermostat to conserve energy",
-    score: 20
+    score: 20,
   },
   {
     action: "Avoiding single-use plastics",
-    score: 10
+    score: 10,
   },
   {
     action: "Supporting sustainable farming and fishing practices",
-    score: 20
+    score: 20,
   },
   {
     action: "Using non-toxic cleaning products",
-    score: 10
+    score: 10,
   },
   {
-    action: "Supporting politicians and policies that prioritize environmental protection",
-    score: 40
+    action:
+      "Supporting politicians and policies that prioritize environmental protection",
+    score: 40,
   },
   {
     action: "Participating in community clean-up events",
-    score: 30
+    score: 30,
   },
   {
-    action: "Using a bidet or reusable cloth wipes instead of disposable toilet paper",
-    score: 20
+    action:
+      "Using a bidet or reusable cloth wipes instead of disposable toilet paper",
+    score: 20,
   },
   {
     action: "Reducing meat consumption and adopting a plant-based diet",
-    score: 30
+    score: 30,
   },
   {
-    action: "Using a reusable water bottle instead of disposable plastic bottles",
-    score: 10
+    action:
+      "Using a reusable water bottle instead of disposable plastic bottles",
+    score: 10,
   },
   {
-    action: "Taking public transportation, biking or walking instead of driving a car",
-    score: 20
+    action:
+      "Taking public transportation, biking or walking instead of driving a car",
+    score: 20,
   },
   {
     action: "Planting a tree or starting a garden",
-    score: 30
+    score: 30,
   },
   {
     action: "Using energy-efficient appliances and light bulbs",
-    score: 10
+    score: 10,
   },
   {
     action: "Buying organic and locally-sourced foods",
-    score: 20
+    score: 20,
   },
   {
     action: "Composting food waste instead of throwing it away",
-    score: 30
+    score: 30,
   },
   {
     action: "Installing solar panels or using other renewable energy sources",
-    score: 40
+    score: 40,
   },
   {
     action: "Using cloth bags instead of plastic bags",
-    score: 10
+    score: 10,
   },
   {
     action: "Turning off lights and electronics when not in use",
-    score: 10
+    score: 10,
   },
   {
     action: "Supporting companies with eco-friendly practices and policies",
-    score: 20
-  }
+    score: 20,
+  },
 ];
-
-

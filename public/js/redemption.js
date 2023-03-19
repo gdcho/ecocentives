3012b4d8f9a81833b3ec7236ba06711d414a696b
Gin Lu <glu23@my.bcit.ca>
2023-03-18 23:50:55 -0700

    Updating redemption.html

--- public/js/redemption.js
+++ public/js/redemption.js
@@ -1,50 +1,62 @@
-var redemptionDocID = localStorage.getItem("redemptionDocID");
-function getRedemptionID(id) {
-    db.collection("redeemed")
-        .doc(id)
-        .get()
-        .then((thisRedemption) => {
-            var redemptionID = thisRedemption.data().name;
-            document.getElementById("redemptionName").innerHTML = redemptionID;
-        });
-}
-getRedemptionID(redemptionDocID);
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    const userRef = db.collection("users").doc(user.uid);
+
+    userRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const userData = doc.data();
+          // Populate the form fields with the user's data.
+          document.getElementById("nameInput").value = userData.name;
+          document.getElementById("emailInput").value = userData.email;
+        } else {
+          console.log("No such document!");
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+  } else {
+    console.log("No user is currently logged in!");
+  }
+});
 
 function submitRedemption() {
-    console.log("Confirm point redemption");
+  console.log("Confirm point redemption");
 
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            var currentUser = db.collection("users").doc(user.uid);
-            var userID = user.uid;
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
 
-            //get the document for current user.
-            currentUser.get()
-                .then(userDoc => {
-                    var userEmail = userDoc.data().email;
-                    var phone = document.getElementById("phoneInput").value;
-                    var redemptionData = {
-                        redemptionDocID: redemptionDocID,
-                        userID: userEmail,
-                        phone: phone,
-                        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-                    };
-                    db.collection("redeemed").add(redemptionData)
-                        .then(() => {
-                            console.log("Document successfully written!");
-                            window.location.href = "/html/confirmation.html";
-                        })
-                        .catch(error => {
-                            console.error("Error writing document: ", error);
-                        });
-                })
-                .catch(error => {
-                    console.error("Error getting document: ", error);
-                });
-        } else {
-            console.log("No user is signed in");
-            window.location.href = 'redemption.html';
-        }
-    });
+      currentUser
+        .get()
+        .then((userDoc) => {
+          var userEmail = userDoc.data().email;
+          var phone = document.getElementById("phoneInput").value;
+          var redemptionData = {
+            redemptionDocID: redemptionDocID,
+            userID: userEmail,
+            phone: phone,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+          };
+          db.collection("redeemed")
+            .add(redemptionData)
+            .then(() => {
+              console.log("Document successfully written!");
+              window.location.href = "/html/confirmation.html";
+            })
+            .catch((error) => {
+              console.error("Error writing document: ", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error getting document: ", error);
+        });
+    } else {
+      console.log("No user is signed in");
+      window.location.href = "redemption.html";
+    }
+  });
 }
-

4b8bd097ef2c2e816ca77918ce1e711e06282c23
Sohee Hwang <shwang67@my.bcit.ca>
2023-03-23 11:05:07 -0700

    sohee

--- public/js/skeleton.js
+++ public/js/skeleton.js
@@ -52,11 +52,24 @@ loadSkeleton(); //invoke the function
   //------------------------------------------------
 // Call this function when the "logout" button is clicked
 //-------------------------------------------------
+// function logout() {
+//     firebase.auth().signOut().then(() => {
+//         // Sign-out successful.
+//         console.log("logging out user");
+//         window.location.href = 'login.html';
+//       }).catch((error) => {
+//         // An error happened.
+//       });
+// }
+
+//------------------------------------------------
+// Call this function when the "logout" button is clicked
+//-------------------------------------------------
 function logout() {
     firebase.auth().signOut().then(() => {
         // Sign-out successful.
         console.log("logging out user");
-        window.location.href = 'login.html';
+        // window.location.href = 'index.html';
       }).catch((error) => {
         // An error happened.
       });

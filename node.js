39aad1c2b4bf373af6d3b2ab13b3977595675bdf
David Cho <gcho13@my.bcit.ca>
2023-04-01 02:12:50 -0700

    appfooter - work in progress

--- node.js
+++ node.js
@@ -82,6 +82,10 @@ app.get('/nav_after_login.html', (req, res) => {
   res.sendFile(__dirname + '/text/nav_after_login.html');
 });
 
+app.get('/footer_after_login.html', (req, res) => {
+  res.sendFile(__dirname + '/text/footer_after_login.html');
+});
+
 // For resource not found (i.e., 404)
 app.use(function (req, res, next) {
   res.status(404).send("<html><head><title>Page not found!</title></head><body><p>Nothing here.</p></body></html>");

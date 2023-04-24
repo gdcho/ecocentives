069e3f5c6776dea5fa6aeeea3c72c12c048f3a27
David Cho <48280799+gdcho@users.noreply.github.com>
2023-04-24 13:24:38 -0700

    removed API key

--- public/js/firebaseAPI_team10.js
+++ public/js/firebaseAPI_team10.js
@@ -1,13 +1,13 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-  apiKey: "AIzaSyDRDpX0J5MhrUoXb3IrLe8d2nRa10KC1Gs",
-  authDomain: "comp1800-202310-3a4f1.firebaseapp.com",
-  projectId: "comp1800-202310-3a4f1",
-  storageBucket: "comp1800-202310-3a4f1.appspot.com",
-  messagingSenderId: "98352856971",
-  appId: "1:98352856971:web:8937db7ec1c6c5d88b798e",
+var firebaseConfig = { //Input firebase API keys
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 //--------------------------------------------
